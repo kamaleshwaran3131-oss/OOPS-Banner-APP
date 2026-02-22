da1a26634019c8c2f8d869e6abceb4d5addfb335
kamaleshwaran3131-oss <kamaleshwaran3131@gmail.com>
2026-02-22 21:55:18 +0530

    uc3 completed

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,25 +1,26 @@
 /**
- * OOPSBannerApp UC2 - Banner Display Application
- * * This class displays a stylized "OOPS" banner in the console.
- * The spacing is carefully calibrated to match the visual layout 
+ * OOPSBannerApp UC3 - Banner Display Application
+ *
+ * This class displays a stylized "OOPS" banner in the console.
+ * The spacing is carefully calibrated to match the visual layout
  * of the provided reference image.
- * * @author Developer
+ *
+ * @author Developer
  * @version 2.1
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        
-        // Line 1: Top caps of O, O, P and S
-        System.out.println("   ****    " + " " + "   ****    " + " " + "**********  "+ " " + "**********");
-        System.out.println(" **     ** " + " " + " **     ** " + " " + "*********** "+ " " + "**********");
-        System.out.println("**       **" + " " + "**       **" + " " + "**        **"+ " " + "**        ");
-        System.out.println("**       **" + " " + "**       **" + " " + "**        **"+ " " + "**        ");
-        System.out.println("**       **" + " " + "**       **" + " " + "**       ** "+ " " + "**********");
-        System.out.println("**       **" + " " + "**       **" + " " + "*********   "+ " " + "**********");
-        System.out.println("**       **" + " " + "**       **" + " " + "***         "+ " " + "        **");
-        System.out.println(" **     ** " + " " + " **     ** " + " " + "***         "+ " " + "        **");
-        System.out.println("   ****    " + " " + "   ****    " + " " + "***         "+ " " + "**********");
 
+        // Line 1: Top caps of O, O, P and S
+        System.out.println(String.join(" ","   ****    ","   ****    ","**********  ","**********"));
+        System.out.println(String.join(" "," **     ** "," **     ** ","*********** ","**********"));
+        System.out.println(String.join(" ","**       **","**       **","**        **","**        "));
+        System.out.println(String.join(" ","**       **","**       **","**        **","**        "));
+        System.out.println(String.join(" ","**       **","**       **","**       ** ","**********"));
+        System.out.println(String.join(" ","**       **","**       **","**********  ","**********"));
+        System.out.println(String.join(" ","**       **","**       **","***         ","        **"));
+        System.out.println(String.join(" "," **     ** "," **     ** ","***         ","        **"));
+        System.out.println(String.join(" ","   ****    ","   ****    ","***         ","**********"));
     }
 }
